5f026d25fd5ede872c3c0d6eb2c3c30fd8d6db70
Zhang Kaiyu <yeannylam@gmail.com>
2019-07-24 09:58:00 +0800

    update API

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,12 +1,54 @@
-import { optionsToUsage } from '../src/index';
 import chalk from 'chalk';
+import { body, options, title } from '../src/index';
 
-describe("optionsToUsage: ", () => {
+describe('title: ', () => {
+
+  it("format as title: ", () => {
+    const usage = title("Usage");
+    expect(usage).toBe('\n' + chalk.bold.underline('Usage') + '\n\n');
+  });
+
+});
+
+describe('body: ', () => {
+
+  it('print plain text with new line if very short.', () => {
+    const content = body('The app is awesome.');
+    expect(content).toBe('The app is awesome.\n');
+  });
+
+  it('print plain text with new line if very short.', () => {
+    const content = body('The app is awesome.');
+    expect(content).toBe('The app is awesome.\n');
+  });
+
+  it('print plain text with leading spaces.', () => {
+    const content = body('The app is awesome.', 2);
+    expect(content).toBe('  The app is awesome.\n');
+  });
+
+  it("doesn't print trailing spaces for one line content.", () => {
+    const content = body('The app is awesome.', 2, 2);
+    expect(content).toBe('  The app is awesome.\n');
+  });
+
+  it('wrap lines.', () => {
+    const content = body('The app is awesome. The app usage is very very very long. The app has a lot of complex features.');
+    expect(content).toBe('The app is awesome. The app usage is very very very long. The app has a lot of c\nomplex features.\n');
+  });
+
+  it('wrap lines with spaces.', () => {
+    const content = body('The app is awesome. The app usage is very very very long. The app has a lot of complex features.', 2, 2);
+    expect(content).toBe('  The app is awesome. The app usage is very very very long. The app has a lot   \n  of complex features.\n');
+  });
+});
+
+describe("options: ", () => {
 
   describe("handle argument format: ", () => {
 
     it("string", () => {
-      const usage = optionsToUsage({
+      const usage = options({
         file: {
           type: 'string',
           desc: 'the file input.'
@@ -18,7 +60,7 @@ describe("optionsToUsage: ", () => {
     });
 
     it("string[]", () => {
-      const usage = optionsToUsage({
+      const usage = options({
         files: {
           type: 'string[]',
           desc: 'the files input.'
@@ -30,7 +72,7 @@ describe("optionsToUsage: ", () => {
     });
 
     it("number", () => {
-      const usage = optionsToUsage({
+      const usage = options({
         age: {
           type: 'number',
           desc: 'the age of the user.'
@@ -42,7 +84,7 @@ describe("optionsToUsage: ", () => {
     });
 
     it("number[]", () => {
-      const usage = optionsToUsage({
+      const usage = options({
         ages: {
           type: 'number[]',
           desc: 'the ages of the users.'
@@ -54,7 +96,7 @@ describe("optionsToUsage: ", () => {
     });
 
     it("boolean", () => {
-      const usage = optionsToUsage({
+      const usage = options({
         power: {
           type: 'boolean',
           desc: 'whether use power output.'
@@ -68,7 +110,7 @@ describe("optionsToUsage: ", () => {
   });
 
   it("display alias if it's present.", () => {
-    const usage = optionsToUsage({
+    const usage = options({
       power: {
         type: 'boolean',
         alias: 'p',
@@ -81,7 +123,7 @@ describe("optionsToUsage: ", () => {
   });
 
   it("put spaces before the line if leading spaces are present.", () => {
-    const usage = optionsToUsage({
+    const usage = options({
       power: {
         type: 'boolean',
         desc: 'whether use power output.'
@@ -93,7 +135,7 @@ describe("optionsToUsage: ", () => {
   });
 
   it("aligns description.", () => {
-    const usage = optionsToUsage({
+    const usage = options({
       help: {
         type: 'boolean',
         desc: 'display help.'
@@ -115,7 +157,7 @@ describe("optionsToUsage: ", () => {
   });
 
   it("wraps long descriptions into the next line.", () => {
-    const usage = optionsToUsage({
+    const usage = options({
       version: {
         type: 'boolean',
         desc: 'Display version.'
@@ -125,7 +167,6 @@ describe("optionsToUsage: ", () => {
         desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
       }
     });
-    console.log(usage);
     expect(usage).toBe(
       chalk.bold('--version') + ' '.repeat(23) + 'Display version.\n' +
       chalk.bold('--very-very-very-long') + chalk.dim(' ' + chalk.underline('number')) + ' '.repeat(4) + `Lorem ipsum dolor sit amet, consectetur adipisic\n${' '.repeat(32)}ing elit, sed do eiusmod tempor incididunt ut la\n${' '.repeat(32)}bore et dolore magna aliqua. Ut enim ad minim ve\n${' '.repeat(32)}niam, quis nostrud exercitation ullamco laboris \n${' '.repeat(32)}nisi ut aliquip ex ea commodo consequat.`
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,56 @@
-import { OptionRules } from 'type-args';
 import * as slugify from '@sindresorhus/slugify';
 import chalk from 'chalk';
 import * as stringLength from 'string-length';
+import { OptionRules } from 'type-args';
 
-export const optionsToUsage = function(
+export const title = function(content: string): string {
+  return '\n' + chalk.bold.underline(content) + '\n\n';
+}
+
+export const body = function(
+  content: string,
+  leadingSpaces: number = 0,
+  trailingSpaces: number = 0,
+  lineWidth: number = Math.min(process.stdout.columns || 80, 80)
+) {
+  const effectiveWidth = lineWidth - leadingSpaces - trailingSpaces;
+  const lines = content.match(new RegExp(`.{1,${effectiveWidth}}`, 'g')) as string[];
+  return ' '.repeat(leadingSpaces) + lines.join(' '.repeat(trailingSpaces) + '\n' + ' '.repeat(leadingSpaces)) + '\n';
+}
+
+export const options = function(
   optionRules: OptionRules,
   leadingSpaces: number = 0,
   lineWidth: number = Math.min(process.stdout.columns || 80, 80)
 ): string {
-  let left = {}, values = {};
+  const left = {};
+  const values = {};
   for (const key in optionRules) {
-    left[key] = '';
-    const rule = optionRules[key];
-    left[key] += ' '.repeat(leadingSpaces);
-    if (rule.alias) {
-      left[key] += chalk.bold(`-${rule.alias}, `);
-    }
-    left[key] += chalk.bold(`--${slugify(key)}`);
+    if (optionRules.hasOwnProperty(key)) {
+      left[key] = '';
+      const rule = optionRules[key];
+      left[key] += ' '.repeat(leadingSpaces);
+      if (rule.alias) {
+        left[key] += chalk.bold(`-${rule.alias}, `);
+      }
+      left[key] += chalk.bold(`--${slugify(key)}`);
 
-    switch (rule.type) {
-      case 'string':
-        left[key] += chalk.dim(' ' + chalk.underline('string'));
-        break;
-      case 'string[]':
-        left[key] +=  chalk.dim(' ' + chalk.underline('string') + ' ...');
-        break;
-      case 'number':
-        left[key] +=  chalk.dim(' ' + chalk.underline('number'));
-        break;
-      case 'number[]':
-        left[key] +=  chalk.dim(' ' + chalk.underline('number') + ' ...');
-        break;
-      case 'boolean':
-        break;
+      switch (rule.type) {
+        case 'string':
+          left[key] += chalk.dim(' ' + chalk.underline('string'));
+          break;
+        case 'string[]':
+          left[key] +=  chalk.dim(' ' + chalk.underline('string') + ' ...');
+          break;
+        case 'number':
+          left[key] +=  chalk.dim(' ' + chalk.underline('number'));
+          break;
+        case 'number[]':
+          left[key] +=  chalk.dim(' ' + chalk.underline('number') + ' ...');
+          break;
+        case 'boolean':
+          break;
+      }
     }
   }
   const longest = (Object.values(left) as string[]).reduce((p, c) => {
@@ -40,10 +58,12 @@ export const optionsToUsage = function(
   }, 0);
   const targetLength = longest + 4;
   for (const key in optionRules) {
-    const padding = ' '.repeat(targetLength - stringLength(left[key]));
-    const descLength = lineWidth - targetLength;
-    const desc = (optionRules[key].desc.match(new RegExp(`.{1,${descLength}}`, 'g')) as string[]).join(`\n${' '.repeat(targetLength)}`);
-    values[key] = left[key] + padding + desc;
+    if (optionRules.hasOwnProperty(key)) {
+      const padding = ' '.repeat(targetLength - stringLength(left[key]));
+      const descLength = lineWidth - targetLength;
+      const desc = (optionRules[key].desc.match(new RegExp(`.{1,${descLength}}`, 'g')) as string[]).join(`\n${' '.repeat(targetLength)}`);
+      values[key] = left[key] + padding + desc;
+    }
   }
   return (Object.values(values) as string[]).join('\n');
 };
